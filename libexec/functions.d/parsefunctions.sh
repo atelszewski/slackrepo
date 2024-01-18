6872cecb7888e7923e45795ae38329f419c68102
Andrzej Telszewski <andrzej@telszewski.com>
2024-01-18 10:04:50 +0100

    Fix that chroot is not properly tested for user and group
    When testing if the required user and/ or group exist,
    the check was performed on the main filesystem and not
    in chroot if one was provided.
    
    This was causing failures if the required user or group
    were present on the main filesystem, but not in chroot.
    
    With this change, the test for user and group is performed
    in chroot if one is provided, or in the main filesystem
    otherwise.

--- libexec/functions.d/parsefunctions.sh
+++ libexec/functions.d/parsefunctions.sh
@@ -262,6 +262,25 @@ declare -A VALID_USERS VALID_GROUPS
 
 #-------------------------------------------------------------------------------
 
+function chroot_getent
+# Run `getent`, possibly in a chroot if one is provided.
+# $* = arguments as expected by the regular `getent` command
+# Return status: Normally, the status from `getent` is returned.
+# If `sudo` or `chroot` fails, then their status is returned instead.
+{
+  if [ "$OPT_CHROOT" = 'n' ]; then
+    getent "${@}"
+  else
+    if [ -z "${SUDO}" ]; then
+        chroot $OPT_CHROOT getent "${@}"
+    else
+        ${SUDO} chroot --userspec=${USER} $OPT_CHROOT getent "${@}"
+    fi
+  fi
+}
+
+#-------------------------------------------------------------------------------
+
 function parse_info_and_hints
 # Load up .info file into variables INFO*, and hints into variables HINT_*
 # Also populates SRCDIR, GITREV and GITDIRTY
@@ -493,7 +512,7 @@ function parse_info_and_hints
           esac
         done
         [ -z "$gnum" ] && { log_warning -n "${itemid}: GROUPADD hint has no GID number" ; break ; }
-        if ! getent group "$gname" | grep -q "^${gname}:" 2>/dev/null ; then
+        if ! chroot_getent group "$gname" | grep -q "^${gname}:" 2>/dev/null ; then
           HINT_GROUPADD[$itemid]="${HINT_GROUPADD[$itemid]}groupadd -g $gnum $gname; "
         else
           log_info -a "Group $gname already exists."
@@ -522,7 +541,7 @@ function parse_info_and_hints
           esac
         done
         [ -z "$unum" ] && { log_warning -n "${itemid}: USERADD hint has no UID number" ; break ; }
-        if ! getent passwd "$uname" | grep -q "^${uname}:" 2>/dev/null ; then
+        if ! chroot_getent passwd "$uname" | grep -q "^${uname}:" 2>/dev/null ; then
           [ -z "$ugroup" ] && ugroup="$uname"
           HINT_USERADD[$itemid]="${HINT_USERADD[$itemid]}useradd  -u $unum -g $ugroup -c $itemprgnam -d $udir -s $ushell $uargs $uname; "
         else
